0a4c0a7aaf6edcfca6e6a08d21b41265c43f2540
Jorge Calderon <jcalderoxp@alumnos.unex.es>
2025-06-20 11:39:29 +0200

    Trying to do stable the P3Bot two arm

--- roboticstoolbox/models/URDF/P3Bot.py
+++ roboticstoolbox/models/URDF/P3Bot.py
@@ -3,7 +3,7 @@
 import numpy as np
 from roboticstoolbox.robot.Robot import Robot
 from statsmodels.genmod.families import links
-
+from roboticstoolbox.robot.Link import Link
 
 class P3Bot(Robot):
     """
@@ -23,15 +23,35 @@ class P3Bot(Robot):
     """
 
     def __init__(self):
-
         links, name, urdf_string, urdf_filepath = self.URDF_read(
             "p3bot_description/urdf/P3Bot_scaled.urdf"
         )
+
+        # gripper_r_base = links[16]
+        # gripper_l_base = links[29]
+
+        # # Find the finger links
+        # r_gripper_links = [link for link in links if link.parent == gripper_r_base]
+        # l_gripper_links = [link for link in links if link.parent == gripper_l_base]
+
+        # # New intermediate links
+        # r_gripper = Link(name="r_gripper", parent=gripper_l_base)
+        # l_gripper = Link(name="l_gripper", parent=gripper_r_base)
+        # links.append(r_gripper)
+        # links.append(l_gripper)
+
+        # # Set the finger link parent to be the new gripper base link
+        # for g_link in r_gripper_links:
+        #     g_link._parent = r_gripper
+
+        # for g_link in l_gripper_links:
+        #     g_link._parent = l_gripper
 
         super().__init__(
             links,
             name=name,
             manufacturer="Robolab",
+            # gripper_links=[r_gripper, l_gripper],
             urdf_string=urdf_string,
             urdf_filepath=urdf_filepath,
         )
